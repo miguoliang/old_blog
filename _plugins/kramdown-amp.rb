6b067825cede33eaeb5cbf29e866c7f6e15e7322
米国梁 <nothingmi@muchencute.com>
2019-11-25 16:44:32 +0800

    update amp-img

--- _plugins/kramdown-amp.rb
+++ _plugins/kramdown-amp.rb
@@ -8,9 +8,9 @@ module Kramdown
                 img_src = img_src.slice(1..-1) if img_src.start_with?("/")
 
                 if img_src.end_with?(".jpg") || img_src.end_with?(".png")
-                    "<amp-img layout='container' src='#{el.attr['src']}' alt='#{el.attr['alt']}' width='535' height='300'><amp-img#{html_attributes(el.attr)} width='535' height='300'></amp-img></amp-img>"
+                    "<amp-img layout='responsive' src='#{el.attr['src']}' alt='#{el.attr['alt']}' width='535' height='300'><amp-img fallback layout='responsive' #{html_attributes(el.attr)} width='535' height='300'></amp-img></amp-img>"
                 else
-                    "<amp-img layout='container' #{html_attributes(el.attr)} width='535' height='300' layout='responsive'></amp-img>"
+                    "<amp-img layout='responsive' #{html_attributes(el.attr)} width='535' height='300' layout='responsive'></amp-img>"
                 end
             end
         end
