069e9164a2f125531c0c264a3fc2dceb4af77553
米国梁 <nothingmi@muchencute.com>
2019-11-25 16:48:44 +0800

    update images

--- _plugins/kramdown-amp.rb
+++ _plugins/kramdown-amp.rb
@@ -6,11 +6,12 @@ module Kramdown
             def convert_img(el, indent)
                 img_src = el.attr["src"]
                 img_src = img_src.slice(1..-1) if img_src.start_with?("/")
+                w, h = FastImage.size(img_src)
 
                 if img_src.end_with?(".jpg") || img_src.end_with?(".png")
-                    "<amp-img layout='responsive' src='#{el.attr['src']}' alt='#{el.attr['alt']}' width='535' height='300'><amp-img fallback layout='responsive' #{html_attributes(el.attr)} width='535' height='300'></amp-img></amp-img>"
+                    "<amp-img layout='responsive' src='#{el.attr['src']}' alt='#{el.attr['alt']}' width='#{w}' height='#{h}'><amp-img fallback layout='responsive' #{html_attributes(el.attr)} width='#{w}' height='#{h}'></amp-img></amp-img>"
                 else
-                    "<amp-img layout='responsive' #{html_attributes(el.attr)} width='535' height='300' layout='responsive'></amp-img>"
+                    "<amp-img layout='responsive' #{html_attributes(el.attr)} width='535' height='#{h}' layout='responsive'></amp-img>"
                 end
             end
         end
