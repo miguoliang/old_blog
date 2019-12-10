90888b5ae5a88e588617b54f537e2e0f151fcc0d
米国梁 <nothingmi@muchencute.com>
2019-12-10 10:52:12 +0800

    update scripts

--- __scripts/generate_webp.sh
+++ __scripts/generate_webp.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/sh
 
+ls assets/**/banner.jpg | xargs -t -I {} convert {} -resize 1200x1200! {}_1x1.jpg
+
+## 将 JPG 转换为 WebP 格式，同时保留原 JPG，保真度 80%
 ls assets/**/*.jpg | xargs -t -I {} cwebp -q 80 {} -o {}.webp
