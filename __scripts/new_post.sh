8255b2a7832448046a3d096d98a7f1055f02b10b
米国梁 <nothingmi@muchencute.com>
2020-02-24 18:40:59 +0800

    增加新建文章自动脚本

---
+++ __scripts/new_post.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+## 当前日期
+TODAY=`date +%Y-%m-%d`
+
+## 将空格转化成减号
+TITLE=`echo $2 | tr '[A-Z]' '[a-z]' | sed 's/ /-/g'`
+
+## 按照空格分割并转化成数组
+WORDS=($2)
+
+## 创建模版文件
+echo "---
+layout: post
+title: "$2"
+description:
+image: /assets/$TODAY-$TITLE/banner.jpg
+categories:
+    - $1
+tags:
+    -
+---
+" > "$1/_posts/$TODAY-$TITLE.md"
+
+## 创建图片目录
+mkdir -p "assets/$TODAY-$TITLE"
+
+## 生成图片
+magick -size 1200x1200 -background green -fill white -gravity center -font __scripts/arial.ttf label:"$2" "assets/$TODAY-$TITLE/banner.jpg"
